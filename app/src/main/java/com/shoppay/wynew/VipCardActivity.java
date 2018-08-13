2c0243c4e5639447814a8cfb08382cdf2375b491
yy471101598 <471101598@qq.com>
2018-08-14 00:05:32 +0800

    1222

--- app/src/main/java/com/shoppay/wynew/VipCardActivity.java
+++ app/src/main/java/com/shoppay/wynew/VipCardActivity.java
@@ -1,12 +1,16 @@
 package com.shoppay.wynew;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
 import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.os.Handler;
@@ -42,11 +46,13 @@ import com.shoppay.wynew.tools.PreferenceHelper;
 import org.json.JSONObject;
 
 import java.lang.reflect.Type;
+import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -54,7 +60,7 @@ import cz.msebera.android.httpclient.Header;
  * Created by songxiaotao on 2017/6/30.
  */
 
-public class VipCardActivity extends BaseNfcActivity implements View.OnClickListener {
+public class VipCardActivity extends Activity implements View.OnClickListener {
     private RelativeLayout rl_left, rl_save, rl_boy, rl_girl, rl_vipdj;
     private EditText et_vipcard, et_bmcard, et_vipname, et_phone, et_tjcard;
     private TextView tv_title, tv_boy, tv_girl, tv_vipsr, tv_vipdj, tv_tjname, tv_endtime;
@@ -64,6 +70,12 @@ public class VipCardActivity extends BaseNfcActivity implements View.OnClickList
     private Dialog dialog;
     private List<Dengji> list;
     private Dengji dengji;
+    private static final DateFormat TIME_FORMAT = SimpleDateFormat
+            .getDateTimeInstance();
+    private NfcAdapter mAdapter;
+    private PendingIntent mPendingIntent;
+    private NdefMessage mNdefPushMessage;
+
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -88,6 +100,17 @@ public class VipCardActivity extends BaseNfcActivity implements View.OnClickList
         dialog = DialogUtil.loadingDialog(VipCardActivity.this, 1);
         ActivityStack.create().addActivity(VipCardActivity.this);
         initView();
+        resolveIntent(getIntent());
+
+        // 获取默认的NFC控制器
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        //拦截系统级的NFC扫描，例如扫描蓝牙
+        mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        mNdefPushMessage = new NdefMessage(new NdefRecord[] { newTextRecord("",
+                Locale.ENGLISH, true) });
+
         vipDengjiList("no");
 
         et_tjcard.addTextChangedListener(new TextWatcher() {
@@ -116,6 +139,234 @@ public class VipCardActivity extends BaseNfcActivity implements View.OnClickList
         });
 
     }
+
+
+    private NdefRecord newTextRecord(String text, Locale locale,
+                                     boolean encodeInUtf8) {
+        byte[] langBytes = locale.getLanguage().getBytes(
+                Charset.forName("US-ASCII"));
+
+        Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset
+                .forName("UTF-16");
+        byte[] textBytes = text.getBytes(utfEncoding);
+
+        int utfBit = encodeInUtf8 ? 0 : (1 << 7);
+        char status = (char) (utfBit + langBytes.length);
+
+        byte[] data = new byte[1 + langBytes.length + textBytes.length];
+        data[0] = (byte) status;
+        System.arraycopy(langBytes, 0, data, 1, langBytes.length);
+        System.arraycopy(textBytes, 0, data, 1 + langBytes.length,
+                textBytes.length);
+
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT,
+                new byte[0], data);
+    }
+
+
+    //初步判断是什么类型NFC卡
+    private void resolveIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent
+                    .getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                // Unknown tag type
+                byte[] empty = new byte[0];
+                byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+                Parcelable tag = intent
+                        .getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                byte[] payload = dumpTagData(tag).getBytes();
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN,
+                        empty, id, payload);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] { record });
+                msgs = new NdefMessage[] { msg };
+            }
+            // Setup the views
+            Log.d("xxNFC",new Gson().toJson(msgs));
+            NdefRecord record = msgs[0].getRecords()[0];
+            String textRecord = parseTextRecord(record);
+            Log.d("xxNFCMSG",textRecord);
+        }
+    }
+    //一般公家卡，扫描的信息
+    private String dumpTagData(Parcelable p) {
+        StringBuilder sb = new StringBuilder();
+        Tag tag = (Tag) p;
+        byte[] id = tag.getId();
+        sb.append("Tag ID (hex): ").append(getHex(id)).append("\n");
+        sb.append("Tag ID (dec): ").append(getDec(id)).append("\n");
+        sb.append("ID (reversed): ").append(getReversed(id)).append("\n");
+
+        String prefix = "android.nfc.tech.";
+        sb.append("Technologies: ");
+        for (String tech : tag.getTechList()) {
+            sb.append(tech.substring(prefix.length()));
+            sb.append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length());
+        for (String tech : tag.getTechList()) {
+            if (tech.equals(MifareClassic.class.getName())) {
+                sb.append('\n');
+                MifareClassic mifareTag = MifareClassic.get(tag);
+                String type = "Unknown";
+                switch (mifareTag.getType()) {
+                    case MifareClassic.TYPE_CLASSIC:
+                        type = "Classic";
+                        break;
+                    case MifareClassic.TYPE_PLUS:
+                        type = "Plus";
+                        break;
+                    case MifareClassic.TYPE_PRO:
+                        type = "Pro";
+                        break;
+                }
+                sb.append("Mifare Classic type: ");
+                sb.append(type);
+                sb.append('\n');
+
+                sb.append("Mifare size: ");
+                sb.append(mifareTag.getSize() + " bytes");
+                sb.append('\n');
+
+                sb.append("Mifare sectors: ");
+                sb.append(mifareTag.getSectorCount());
+                sb.append('\n');
+
+                sb.append("Mifare blocks: ");
+                sb.append(mifareTag.getBlockCount());
+            }
+
+            if (tech.equals(MifareUltralight.class.getName())) {
+                sb.append('\n');
+                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
+                String type = "Unknown";
+                switch (mifareUlTag.getType()) {
+                    case MifareUltralight.TYPE_ULTRALIGHT:
+                        type = "Ultralight";
+                        break;
+                    case MifareUltralight.TYPE_ULTRALIGHT_C:
+                        type = "Ultralight C";
+                        break;
+                }
+                sb.append("Mifare Ultralight type: ");
+                sb.append(type);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String getHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            int b = bytes[i] & 0xff;
+            if (b < 0x10)
+                sb.append('0');
+            sb.append(Integer.toHexString(b));
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
+    private long getDec(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = 0; i < bytes.length; ++i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+    private long getReversed(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+    //显示NFC扫描的数据
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) {
+            return;
+        }
+        // Parse the first message in the list
+        // Build views for all of the sub records
+        Date now = new Date();
+//        List<ParsedNdefRecord> records = NdefMessageParser.parse(msgs[0]);
+//        final int size = records.size();
+//        for (int i = 0; i < size; i++) {
+//            TextView timeView = new TextView(this);
+//            timeView.setText(TIME_FORMAT.format(now));
+//            ParsedNdefRecord record = records.get(i);
+//            promt.append(record.getViewText());
+//        }
+    }
+
+    //获取系统隐式启动的
+    @Override
+    public void onNewIntent(Intent intent) {
+        setIntent(intent);
+        resolveIntent(intent);
+//        readNfcTag(intent);
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new ReadCardOpt(et_vipcard);
+        if (mAdapter == null) {
+            if (!mAdapter.isEnabled()) {
+               Toast.makeText(ac,"该设备不支持NFC功能",Toast.LENGTH_SHORT).show();
+            }
+
+            return;
+        }
+        if (!mAdapter.isEnabled()) {
+            Toast.makeText(ac,"请在系统设置中先启用NFC功能",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (mAdapter != null) {
+            //隐式启动
+            mAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
+            mAdapter.enableForegroundNdefPush(this, mNdefPushMessage);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAdapter != null) {
+            //隐式启动
+            mAdapter.disableForegroundDispatch(this);
+            mAdapter.disableForegroundNdefPush(this);
+        }
+    }
+
+
+
+
+
+
 
     /**
      * 延迟线程，看是否还有下一个字符输入
@@ -399,11 +650,6 @@ public class VipCardActivity extends BaseNfcActivity implements View.OnClickList
 
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        new ReadCardOpt(et_vipcard);
-    }
 
     @Override
     protected void onStop() {
@@ -488,20 +734,21 @@ public class VipCardActivity extends BaseNfcActivity implements View.OnClickList
         });
     }
 
-
-    String mTagText;
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        //1.获取Tag对象
-        Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-        //2.获取Ndef的实例
-        Ndef ndef = Ndef.get(detectedTag);
-        mTagText = ndef.getType() + "\nmaxsize:" + ndef.getMaxSize() + "bytes\n\n";
-        readNfcTag(intent);
-        et_vipcard.setText(mTagText);
-    }
-
+//
+//    String mTagText;
+//
+//    @Override
+//    public void onNewIntent(Intent intent) {
+//        //1.获取Tag对象
+//        Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+//        //2.获取Ndef的实例
+//        Log.d("xxnfcm","nfc");
+//        Ndef ndef = Ndef.get(detectedTag);
+////        mTagText = ndef.getType() + "\nmaxsize:" + ndef.getMaxSize() + "bytes\n\n";
+//        Log.d("xxnfc",new Gson().toJson(ndef));
+//        readNfcTag(intent);
+//    }
+//
     /**
      * 读取NFC标签文本数据
      */
@@ -522,7 +769,8 @@ public class VipCardActivity extends BaseNfcActivity implements View.OnClickList
                 if (msgs != null) {
                     NdefRecord record = msgs[0].getRecords()[0];
                     String textRecord = parseTextRecord(record);
-                    mTagText += textRecord + "\n\ntext\n" + contentSize + " bytes";
+                    Log.d("xxNFCMSG",textRecord);
+//                    mTagText += textRecord + "\n\ntext\n" + contentSize + " bytes";
                 }
             } catch (Exception e) {
             }
